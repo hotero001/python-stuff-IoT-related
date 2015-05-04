edd92f228616214f6e1a9e824f7c8a5836af1e47
Hector Otero <otero111@hotmail.com>
2015-05-03 18:28:31 -0700

    just tests

--- python_tests.py
+++ python_tests.py
@@ -17,3 +17,14 @@ x.display("dog")
 
 stepPin = [1,2,3,4]
 print stepPin[0:4]
+
+while True:
+	answer = raw_input("Do you want to continue?: ")
+	if answer == "yes":
+		print("ok, carry on then")
+	elif answer == "no":
+		print("ok, later dude")
+		exit()
+	else:
+		print("I will take that as a yes and continue")
+
